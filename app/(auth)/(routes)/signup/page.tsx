5a788f17760a8c3712b36bab0e0a9bf8d19cf7f6
jansari <jansari@entiovi.com>
2024-08-21 09:05:25 +0530

    define the protected routes and public routes using clerk authentication

--- app/(auth)/(routes)/signup/page.tsx
+++
@@ -1,7 +0,0 @@
-const SignupPage = () => {
- return ( <div>
-    <h1>This is the signup page</h1>
- </div> );
-}
- 
-export default SignupPage;
--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,7 +1,8 @@
-import { cn } from "@/lib/utils";
-const state = false;
-// const state = axios.get('/localhost:8080/user')
-export default function Home() {
-  // only login user can access
-  return <h5>This is a protected routes</h5>;
-}
+import { UserButton } from "@clerk/nextjs";
+
+const Home = () => {
+  return (
+   <UserButton afterSignOutUrl="/"/>
+  )
+};
+export default Home;
