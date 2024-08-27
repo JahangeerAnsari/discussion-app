c518d4a88f1028fc14ac2a4d3b9738b2b3dba807
jansari <jansari@entiovi.com>
2024-08-27 21:40:54 +0530

    theme toggle fix into bottom side of the sidebar

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -5,6 +5,8 @@ import NavigationAction from "./navigation-action";
 import { Separator } from "@/components/ui/separator";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import NavigationItem from "./navigation-item";
+import { ModeToggle } from "../mode-toggle";
+import { UserButton } from "@clerk/nextjs";
 
 const NavigationSidebar = async () => {
   const profile = await currentProfile();
@@ -21,24 +23,30 @@ const NavigationSidebar = async () => {
       },
     },
   });
+  console.log("server",servers);
+  
   return (
     <div
-      className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] pt-3
+      className="space-y-4 flex flex-col items-center h-full text-primary bg-gray-300  w-full dark:bg-[#1E1F22] pt-3
   "
     >
       <NavigationAction />
       <Separator className="h-[1px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto " />
       <ScrollArea>
-       {servers.map((server) =>(
-        <div key={server.id} className="mb-">
-          <NavigationItem 
-          id={server.id}
-          name={server.name}
-          imageUrl={server.imageUrl}
-          />
-        </div>
-       ))}
+        {servers.map((server) => (
+          <div key={server.id} className="mb-">
+            <NavigationItem
+              id={server.id}
+              name={server.name}
+              imageUrl={server.imageUrl}
+            />
+          </div>
+        ))}
       </ScrollArea>
+      <div className="pb-3 mt-auto   flex items-center flex-col gap-y-2 absolute bottom-[-1px]">
+        <ModeToggle />
+        <UserButton />
+      </div>
     </div>
   );
 };
