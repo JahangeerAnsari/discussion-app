fae8bec17dbfeaa5ae1afa2917d1c771339630bf
jansari <jansari@entiovi.com>
2024-08-27 11:53:46 +0530

    how layout created for sidebar

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,13 +1,18 @@
-import ServerPage from "./(routes)/servers/[serverId]/page";
+'use client';
+import NavigationSidebar from "@/components/navigation/navigation-sidebar";
+import { useParams } from "next/navigation";
 
 interface MainServerLayoutChildren{
 children: React.ReactNode
 }
 // Layout file name should be layout otherwise it will not work 
 const MainServerLayout = ({children}:MainServerLayoutChildren) => {
+
  return ( 
     <div className="h-full">
        <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
+        {/* this space for sidebar */}
+        <NavigationSidebar/>
        </div>
        <main className="md:pl-[72px] h-full">
          {children}
---
+++ components/navigation/navigation-sidebar.tsx
@@ -0,0 +1,10 @@
+
+const NavigationSidebar = async () => {
+ return (  
+  <div>
+   <h1>hello sidebar</h1>
+  </div>
+ );
+}
+ 
+export default NavigationSidebar;
