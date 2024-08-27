c60eeb8ab1152955d656fd4edfa40b3295423a60
jansari <jansari@entiovi.com>
2024-08-27 12:15:40 +0530

    if you are using backend data to component dont use "use client" to it because your comp is server-comp

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,24 +1,20 @@
-'use client';
 import NavigationSidebar from "@/components/navigation/navigation-sidebar";
-import { useParams } from "next/navigation";
-
-interface MainServerLayoutChildren{
-children: React.ReactNode
+interface MainServerLayoutChildren {
+  children: React.ReactNode;
 }
-// Layout file name should be layout otherwise it will not work 
-const MainServerLayout = ({children}:MainServerLayoutChildren) => {
+// Layout file name should be layout otherwise it will not work
+// if you are using backend data to the componenet and it is wrap please 
 
- return ( 
+const MainServerLayout = ({ children }: MainServerLayoutChildren) => {
+  return (
     <div className="h-full">
-       <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
+      <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
         {/* this space for sidebar */}
-        <NavigationSidebar/>
-       </div>
-       <main className="md:pl-[72px] h-full">
-         {children}
-       </main>
+        <NavigationSidebar />
+      </div>
+      <main className="md:pl-[72px] h-full">{children}</main>
     </div>
   );
-}
- 
+};
+
 export default MainServerLayout;
--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,7 +1,28 @@
 
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+
 const NavigationSidebar = async () => {
+  const profile = await currentProfile()
+  if(!profile){
+   return redirect("/");
+  }
+  // find all the member profile which is part of server 
+  const servers = await db.server.findMany({
+   where:{
+    members:{
+     some:{
+      profileId:profile.id,
+      
+     }
+    }
+   }
+  })
  return (  
-  <div>
+  <div className="space-y-4 flex flex-col items-center h-full
+  text-primary w-full dark:bg-[#1E1F22] pt-3
+  ">
    <h1>hello sidebar</h1>
   </div>
  );
