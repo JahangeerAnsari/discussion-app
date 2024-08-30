9470212eec9d9b30c33fe3626f63f21ffc0646fb
jansari <jansari@entiovi.com>
2024-08-30 11:54:30 +0530

    dropdownItem created for different options like createChannel ,deleteServer

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -2,8 +2,15 @@
 import { ServerWithMembersWithProfiles } from "@/types/types";
 // we want server in which channnel and members include
 import { MemberRole, Server } from "@prisma/client";
-import { DropdownMenu, DropdownMenuTrigger } from "../ui/dropdown-menu";
-import { ChevronDown } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+import { ChevronDown, PlusCircle, Settings, Trash, UserPlus, Users } from "lucide-react";
+import { Separator } from "../ui/separator";
 
 interface ServerHeaderProps {
   server: ServerWithMembersWithProfiles;
@@ -22,10 +29,50 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
         dark:border-neutral-800 border-b-2 hover:bg-zinc-700/10
         dark:hover:bg-zinc-700/50 transition"
         >
-         {server?.name}
-         <ChevronDown className="h-5 w-5 ml-auto"/>
+          {server?.name}
+          <ChevronDown className="h-5 w-5 ml-auto" />
         </button>
       </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]">
+        {isModerator && (
+          <DropdownMenuItem
+            className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointe">
+            Invite People
+            <UserPlus className="h-4 w-4 ml-auto"/>
+          </DropdownMenuItem>
+        )}
+        {isAdmin && (
+          <DropdownMenuItem
+          className="px-3 py-2 text-center cursor-pointe">
+          Server Settings
+          <Settings className="h-4 w-4 ml-auto"/>
+        </DropdownMenuItem>
+        )}
+        {isAdmin && (
+          <DropdownMenuItem
+          className="px-3 py-2 text-center cursor-pointe">
+          Manage Members
+          <Users className="h-4 w-4 ml-auto"/>
+        </DropdownMenuItem>
+        )}
+        {isModerator && (
+          <DropdownMenuItem
+          className="px-3 py-2 text-center cursor-pointe">
+          Create Channel
+          <PlusCircle className="h-4 w-4 ml-auto"/>
+        </DropdownMenuItem>
+        )}
+        {isModerator && (
+          <DropdownMenuSeparator/>
+        )}
+        {isAdmin && (
+          <DropdownMenuItem
+          className="text-rose-500 px-3 py-2 text-center cursor-pointe">
+          Delete Server
+          <Trash className="h-4 w-4 ml-auto"/>
+        </DropdownMenuItem>
+        )}
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 };
