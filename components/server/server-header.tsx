83fd605138b87e5c5aea9cfba647a18d05eea792
jansari <jansari@entiovi.com>
2024-08-30 10:56:46 +0530

    New Server header added with server name with dropdown icon

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -1,20 +1,33 @@
-'use client'
+"use client";
 import { ServerWithMembersWithProfiles } from "@/types/types";
 // we want server in which channnel and members include
 import { MemberRole, Server } from "@prisma/client";
+import { DropdownMenu, DropdownMenuTrigger } from "../ui/dropdown-menu";
+import { ChevronDown } from "lucide-react";
 
-
-interface ServerHeaderProps{
- server:ServerWithMembersWithProfiles ;
- role?:MemberRole 
+interface ServerHeaderProps {
+  server: ServerWithMembersWithProfiles;
+  role?: MemberRole;
 }
-const ServerHeader = ({server,role}:ServerHeaderProps) => {
- console.log("server,role",server,role);
- console.log("");
- 
- return ( 
-  <h1>server header</h1>
+const ServerHeader = ({ server, role }: ServerHeaderProps) => {
+  const isAdmin = role === MemberRole.ADMIN;
+  const isModerator = isAdmin || MemberRole.MODERATOR;
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="focus:outline-none " asChild>
+        <button
+          className="w-full text-md font-semibold px-3 flex
+        items-center h-12 border-neutral-200
+        dark:border-neutral-800 border-b-2 hover:bg-zinc-700/10
+        dark:hover:bg-zinc-700/50 transition"
+        >
+         {server?.name}
+         <ChevronDown className="h-5 w-5 ml-auto"/>
+        </button>
+      </DropdownMenuTrigger>
+    </DropdownMenu>
   );
-}
- 
+};
+
 export default ServerHeader;
