4a1acaa576737f2a40c8d13573c9680be102ece7
jansari <jansari@entiovi.com>
2024-08-31 11:12:52 +0530

    generating new invite code

--- components/modals/invite-modal.tsx
+++ components/modals/invite-modal.tsx
@@ -12,21 +12,45 @@ import { useStoreModal } from "@/hooks/use-modal-store";
 import { Label } from "@/components/ui/label";
 import { useState } from "react";
 import { Input } from "@/components/ui/input";
-import { Copy, RefreshCw } from "lucide-react";
+import { Check, Copy, RefreshCw } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useCurrentOrigin } from "@/hooks/currnet-origin";
+import axios from "axios";
 const InviteModal = () => {
-  const { isOpen, onClose, type, data } = useStoreModal();
+  const { isOpen, onClose, type, data ,onOpen} = useStoreModal();
   const {server} =data
   const isModalOpen = isOpen && type === "inviteServer";
   const origin = useCurrentOrigin();
-  const [inviteCode, setInviteCode] = useState("");
+  const [copied, setCopied] = useState(false);
+  const[isLoading,setIsLoading] = useState(false)
   const inviteUrl = `${origin}/invite/${server?.inviteCode}`;
 
   const handleCloseModal = () => {
     onClose();
   };
+  const handleCopy = () =>{
+    navigator.clipboard.writeText(inviteUrl);
+    setCopied(true);
+    // after 1 sec copy will be false
+    setTimeout(() =>{
+      setCopied(false);
+    },1000)
+  }
 
+  const handleGenerateNewLink = async () =>{
+    try {
+      setIsLoading(true)
+      const response =await axios.patch(`/api/servers/${server?.id}/invite-code`)
+      // immediatly open the modal
+      // it will update the invite code
+        onOpen("inviteServer",{server:response.data})
+    } catch (error:any) {
+        console.log("error on generating new link",error.message);
+        
+    } finally{
+      setIsLoading(false)
+    }
+  }
   return (
     <Dialog open={isModalOpen} onOpenChange={handleCloseModal}>
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
@@ -45,17 +69,22 @@ const InviteModal = () => {
           </Label>
           <div className="flex items-center mt-2 gap-x-2">
             <Input
+            disabled={isLoading}
               className="bg-zinc-300/50 border-0
                      focus-visible:ring-0 text-black 
                      focus-visible:ring-offset-0
                      "
               value={inviteUrl}
             />
-            <Button size="icon">
-              <Copy className="w-4 h-4" />
+            <Button 
+            disabled={isLoading}
+            size="icon" onClick={handleCopy}>
+              {copied ? <Check/> : <Copy className="w-4 h-4" />}
             </Button>
           </div>
           <Button
+          disabled={isLoading}
+          onClick={handleGenerateNewLink}
             variant="link"
             size="sm"
             className="text-xs text-zinc-500 mt-4"
