ab1663d6b0b702f696018efe5d80936cdb9e3b68
jansari <jansari@entiovi.com>
2024-09-27 10:25:43 +0530

    live Socket connection completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from "@/components/provider/theme-provider";
 import { cn } from "@/lib/utils";
 import { ModalProvider } from "@/components/provider/modal-provider";
 import ToastProvider from "@/components/provider/toast-provider";
+import { SocketProvider } from "@/components/provider/socket-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -30,8 +31,10 @@ export default function RootLayout({
             storageKey="discord-theme"
             >
               <ToastProvider/>
+              <SocketProvider>
             <ModalProvider/>  
             {children}
+            </SocketProvider>
              </ThemeProvider>
         </body>
       </html>
